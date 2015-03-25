c3fb0bc05b83b05d749b5042845ec2c2a8ecf5c1
ion-storm <defconoii@gmail.com>
2015-03-25 09:49:27 -0400

    Update init.cmd
    add in my github

--- init.cmd
+++ init.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM - ***********************************************************
-REM - init.cmd - Eric McConkey 2014
+REM - init.cmd - Eric McConkey 2014 & Kyle Weller 2015
 REM - 
 REM - This script will download and execute the latest version
 REM - of cleartmp from github
@@ -10,7 +10,7 @@ setlocal enableextensions
 %~d0
 cd %~p0
 echo Getting the latest version of cleartmp
-wget -O master.zip --no-check-certificate "https://github.com/emcconkey/cleartmp/archive/master.zip"
+wget -O master.zip --no-check-certificate "https://github.com/ion-storm/cleartmp/archive/master.zip"
 7za x master.zip
 copy cleartmp-master\* .
 rmdir /s /q cleartmp-master
